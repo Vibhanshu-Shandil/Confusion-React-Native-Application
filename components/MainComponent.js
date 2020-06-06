54e07a3e6b05eb8c8966ca741ef66189a63e1cdc
Vibhanshu Shandil <vibhanshushandil31@gmail.com>
2020-06-06 13:30:15 +0530

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 
@@ -89,6 +90,24 @@ class Main extends Component {
       }
     }
     );
+
+    const LoginNavigator = createStackNavigator({
+      Login: { screen: Login }
+    }, {
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: <Icon name="menu" size={24}
+                        color= 'white'
+                        onPress={ () => navigation.toggleDrawer() } 
+                    />,
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTitleStyle: {
+            color: "#fff"            
+        },
+        headerTintColor: "#fff"  
+      })
+    });
 
     const HomeNavigator = createStackNavigator({
       Home: { screen: Home }
@@ -179,6 +198,21 @@ class Main extends Component {
     })
 
     const MainNavigator = createDrawerNavigator({
+      Login: 
+        { screen: LoginNavigator,
+          navigationOptions: {
+            title: 'Login',
+            drawerLabel: 'Login',
+            drawerIcon: ({ tintColor, focused }) => (
+              <Icon
+                name='sign-in'
+                type='font-awesome'            
+                size={24}
+                color={tintColor}
+              />
+            ),
+          }
+        },
       Home: 
         { screen: HomeNavigator,
           navigationOptions: {
@@ -272,6 +306,7 @@ class Main extends Component {
           }
         }
   }, {
+    initialRouteName: 'Home',
     drawerBackgroundColor: '#D1C4E9',
     contentComponent: CustomDrawerContentComponent
   });
